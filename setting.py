103ca50cc5fafc1d2f9b0a06ebec1ae34c0f5b71
Chimpicus <caseyboyes@gmail.com>
2022-09-11 21:49:04 +0100

    change of variable name

--- setting.py
+++ setting.py
@@ -4,22 +4,21 @@ import time
 class setting:
   scenario = ""
   def set():
-    settingOption = ""
-    while(settingOption == ""):
+    while(setting.scenario == ""):
       try:
         inputSettingOption = int(input(primaryText + """\
-      > Choose an adventure settingOption by entering the corresponding number:
-        - (1) Nuclear Winter
-        - (2) Z Outbreak
-        - (3) Trumps America
-        - (4) Random settingOption
+> Choose an adventure setting.scenario by entering the corresponding number:
+  - (1) Nuclear Winter
+  - (2) Z Outbreak
+  - (3) Trumps America
+  - (4) Random setting.scenario
         """))
       
         if (inputSettingOption not in (1, 2, 3, 4) ):
-          settingOption = ""
+          setting.scenario = ""
           print(warningText + "That is not a valid option!")
         else:
-          settingOption  = inputSettingOption
+          setting.scenario  = inputSettingOption
           break
 
       except ValueError:
